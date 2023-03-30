30a04c49dc335734ccd6714c11c4a392af6537f3
hasanttonmoy <tonmoyhassan.th@gmail.com>
2023-03-30 14:51:00 -0600

    donee

--- DayMemory.java
+++ DayMemory.java
@@ -1,15 +1,17 @@
 public class DayMemory {
     public static void main(String[] args) {
-        if (args.length < 1) {
-            throw new CommandArgumentNotProvidedException("Translation file not specified.");
+        if (args.length == 0) {
+            throw new CommandArgumentNotProvidedException("No language code provided.");
         }
 
         String languageCode = args[0];
+
         try {
             Translator translator = new Translator(languageCode);
-            System.out.println(translator.translate(3, 8, 2021));
+            String output = translator.translate(3, 8, 2021);
+            System.out.println(output);
         } catch (ArgFileNotFoundException e) {
-            System.err.println("File not found: " + e.getMessage());
+            System.err.println("Error: " + e.getMessage());
         }
     }
 }
